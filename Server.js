b5722743a54644199b1f0dcaf7cea0d7ae8c0eef
johnbuenodev <johnbuenodev@gmail.com>
2023-01-08 00:20:28 -0300

    atualizando fontes procfile, dependencias ...

--- Server.js
+++ Server.js
@@ -1,12 +1,14 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
 
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
+//app.use(express.json());
 app.use(cors());
 
 app.use(function(req,res, next) {
@@ -16,8 +18,18 @@ app.use(function(req,res, next) {
   next();
 });
 
-app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
+//app.use(express.static('www'));
+app.use(express.static(__dirname + '/www'));
+/*
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port: ' + app.get('port'));
+}); */
+
+//const PORT = process.env.PORT || 8080;
+//var port=process.env.PORT;
+app.listen(process.env.PORT || 3000 , function(){
+  //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Node app is running at localhost:" + app.get('port'));
+  //console.log(port);
 });
